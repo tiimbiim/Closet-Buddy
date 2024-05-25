91f48309b5e10266c1616281b743c224c6be53f6
Nathan <nathadn9@uci.edu>
2024-05-25 11:38:20 -0700

    able to switch between wardrobe and outfits tab, now working on buttons

--- ClosetApp/app/src/main/java/com/example/closetapp/MainActivity.java
+++ ClosetApp/app/src/main/java/com/example/closetapp/MainActivity.java
@@ -4,21 +4,40 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.closetapp.databinding.ActivityMainBinding;
+import com.example.closetapp.R;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replacedFragment(new WardrobeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.wardrobe){
+                replacedFragment(new WardrobeFragment());
+            } else if (item.getItemId() == R.id.outfits) {
+                replacedFragment(new OutfitsFragment());
+            }
+            return true;
         });
+    }
+
+    private void replacedFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
