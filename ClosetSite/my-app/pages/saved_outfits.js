24ace4bf37b9f05662522c06bf591db7563e111e
nathannguyen <nathadn9@uci.edu>
2024-05-25 21:48:02 -0700

    centered navigation bar

---
+++ ClosetSite/my-app/pages/saved_outfits.js
@@ -0,0 +1,19 @@
+import styles from "@/styles/Wardrobe.module.css";
+import React, {useEffect} from 'react';
+import Navbar from "@/comps/Navbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+const wardrobe = () => {
+    return (
+        <>
+        <Navbar/>
+        
+        </>
+    )
+}
+
+
+export default wardrobe;
