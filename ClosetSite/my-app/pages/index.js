2302944493a0a3c74a6b63cf742858158b08484b
yejinnsong <yejins6@uci.edu>
2024-05-25 23:44:21 -0700

    updated Home page

--- ClosetSite/my-app/pages/index.js
+++ ClosetSite/my-app/pages/index.js
@@ -1,6 +1,9 @@
 import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/comps/Navbar";
+import { BiCloset } from "react-icons/bi";
+import { GiClothes } from "react-icons/gi";
+import { IoPhonePortraitOutline } from "react-icons/io5";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,8 +11,18 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <h1 class={styles.title}>Welcome to ClosetBuddy!</h1>
-      <p class={styles.description}>Your on the go closet, where you can plan outfits and keep track of clothes</p>
+      <br></br>
+      <h1 className={styles.title}>Welcome to ClosetBuddy!</h1>
+      <br></br>
+      <p className={styles.description}>Your on the go closet, where you can plan outfits and keep track of clothes, all in one place!</p>
+      <br></br>
+      <h2 className={styles.icon}><BiCloset /> <IoPhonePortraitOutline /> <GiClothes /> </h2>
+      <h3 className={styles.description}>What Does Closet Buddy Offer?</h3>
+      <br></br>
+        <li className={styles.list}>Upload photos of the clothes you want to style</li>
+        <li className={styles.list}>Automatically crop out outfits with our Generative AI Tool</li>
+        <li className={styles.list}>Allows you to select different clothes to manually combine outfits</li>
+        <li className={styles.list}>The ability to save your favorite outfit combos, Pinterest-style!</li>
     </>
   );
 }
